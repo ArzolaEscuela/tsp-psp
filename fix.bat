91e2e008654ae0def12ab3a44e7f6378bc6dac3e
Luis Arzola <contact@heisarzola.com>
2019-05-04 00:16:31 -0600

    Fix Bat + Roles Fixer

---
+++ fix.bat
@@ -0,0 +1,7 @@
+fart -i -r *.html images/ tsp-psp/images/
+fart -i -r *.html tsp-psp/tsp-psp/images/ tsp-psp/images/
+fart -i -r *.html assets/ tsp-psp/assets/
+fart -i -r *.html tsp-psp/tsp-psp/assets/ tsp-psp/assets/
+fart -i -r *.html arzolaescuela.github.io/favicon.ico arzolaescuela.github.io/tsp-psp/favicon.ico
+fart -i -r *.html roles/ tsp-psp/roles/
+fart -i -r *.html tsp-psp/tsp-psp/roles/ tsp-psp/roles/
