5377996d12d72e9a35751b6236c763d92f86e7ad
Arzola <contact@heisarzola.com>
2019-05-07 18:02:58 -0600

    Final Version

--- fix.bat
+++ fix.bat
@@ -7,5 +7,3 @@ fart -i -r *.html roles/ tsp-psp/roles/
 fart -i -r *.html tsp-psp/tsp-psp/roles/ tsp-psp/roles/
 fart -i -r *.html phases/ tsp-psp/phases/
 fart -i -r *.html tsp-psp/tsp-psp/phases/ tsp-psp/phases/
-fart -i -r *.html "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>" ""
-fart -i -r *.html "<link rel=\"stylesheet\" href=\"https://arzolaescuela.github.io/tsp-psp/assets/css/just-the-docs.css\">" "<link rel=\"stylesheet\" href=\"https://arzolaescuela.github.io/tsp-psp/assets/css/just-the-docs.css\"><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>"
